83abb81d05e2ba82dabcfb6c02c21aaa44bb969c
Lop-ma <55141590+Lop-ma@users.noreply.github.com>
2023-04-06 23:52:05 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,8 +5,6 @@ import numpy as np
 chat_id = 539377624 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    n = np.arange(1, 41)
-    a = np.array([0] * 40)
-    for i in range(40):
-        a[i] = 2.0 * x[i] / n[i] / n[i]
-    return a.mean()
+    for i in range(len(x)):
+        x[i] = 2.0 * x[i] / 40 / 40
+    return x.mean()
